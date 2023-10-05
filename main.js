0c10a3cdff6ca732299ed569852c4be8211a5261
VyaasSivaan <118091428+VyaasSivaan@users.noreply.github.com>
2023-10-05 19:59:13 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+video="";
+objects=[];
+status="";
+function preload(){
+video=createVideo('video.mp4');
+video.hide();
+
+
+
+}
+function setup(){
+canvas=createCanvas(480,380);
+canvas.center();
+
+}
+function draw(){
+image(video,0,0,480,380);
+if(status!= ""){
+    objectDetector.detect(video,gotResult);
+    for(i=0; i<objects.length; i++)
+    {
+        document.getElementById("status").innerHTML="status-object detected";
+        document.getElementById("number_of_objects").innerHTML="number of objects detected are "+objects.length;
+        fill("#cb7440");
+        percent=floor(objects[i].confidence*100);
+        text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("#1686b6");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }}}
+function gotResult(error,results){
+if(error){
+
+console.error(error);}
+
+else{
+console.log(results);
+objects=results;
+
+}
+
+
+
+}
+
+
+function start(){
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="status- detecting objects";
+}
+status="";
+function modelLoaded(){
+console.log("modelLoaded");
+status=true;
+video.loop();
+video.speed(1);
+video.volume(0);
+
+}
